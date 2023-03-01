45a923f501f24eaa05d26a494a8c3b5669fb3754
DimaRudov <6794666+DimaRudov@users.noreply.github.com>
2023-03-01 12:52:27 -0500

    adding email and invite functionality to members

--- src/components/Cards/CardMembers.vue
+++ src/components/Cards/CardMembers.vue
@@ -14,10 +14,10 @@
 			:data-source="data"
 		>
 			<a-list-item slot="renderItem" slot-scope="item">
-				<a-button slot="actions" type="link">
+				<a-button slot="actions" type="link" @click="sendEmail(item.email)">
 					Email
 				</a-button>
-				<a-button slot="actions" type="link">
+				<a-button slot="actions" type="link" @click="sendInvite(item.email, item.name)">
 					Invite
 				</a-button>
 				<a-list-item-meta
@@ -51,8 +51,19 @@
 			return {
 			}
 		},
+		methods: {
+			sendEmail(email) {
+				console.log('send email')
+				window.open("mailto:"+ email, '_blank').focus();
+			},
+			sendInvite(email, name) {
+				console.log('send invite')
+				var formattedBody = ", \n \nPlease Use the following link to login into your account at Domos to get access to your personal portal. \nhttps://my-building-vue.vercel.app/#/sign-in \n \nThank you, \nYour Board and the Domos Team";
+				var formattedSubject = "Join Your Building Page @ Domos";
+				window.open("mailto:"+ email+"?subject="+encodeURIComponent(formattedSubject)+"&body=Dear "+name+encodeURIComponent(formattedBody), '_blank').focus();
+			}
+		}
 		
 	})
-
 
 </script>
