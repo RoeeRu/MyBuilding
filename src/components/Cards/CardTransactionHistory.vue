ee56271b7056d269cde9687122af5ecafc37bb3f
DimaRudov <6794666+DimaRudov@users.noreply.github.com>
2023-03-04 11:52:30 -0500

    fixing card transaction latest history
    in the dashboard

--- src/components/Cards/CardTransactionHistory.vue
+++ src/components/Cards/CardTransactionHistory.vue
@@ -1,66 +1,28 @@
 <template>
 
 	<!-- Orders History Timeline Card -->
-	<a-card v-if="data" :bordered="false" class="header-solid h-full" :bodyStyle="{paddingTop: '12px',}">
+	<a-card :bordered="false" class="header-solid h-full" :bodyStyle="{paddingTop: '12px',}">
 		<template #title>
 			<h6>Latest Transactions</h6>			
 		</template>
-		<a-timeline  v-if="data " :data-source="data">
-			<span v-if="data[0] && data[0].transaction_amount.type == 1" >
+		<a-timeline  >
+			<div v-for="(transaction, index) in data.slice(0, 6)" :key="index">	
+				<span v-if="transaction.transaction_amount.type == 1" >
 				<a-timeline-item color="green">
 					
-									${{ data[0].transaction_amount.amount }},
-									{{ data[0].details }}
-					<p>{{ data[1].date }}</p>
-				</a-timeline-item>
-			</span><span v-else >
-				<a-timeline-item color="red">
-					-${{ data[0].transaction_amount.amount }},		
-					{{ data[0].details }}
-					<p>{{ data[0].date }}</p>
+									${{ transaction.transaction_amount.amount }},
+									{{ transaction.details }}
+					<p>{{ transaction.date }}</p>
 				</a-timeline-item>
 			</span>
-			<span v-if="data[1] && data[1].transaction_amount.type == 1" >
-				<a-timeline-item color="green">
-					
-									${{ data[1].transaction_amount.amount }},
-									{{ data[1].details }}
-					<p>{{ data[1].date }}</p>
-				</a-timeline-item>
-			</span><span v-else >
+			<span v-else >
 				<a-timeline-item color="red">
-					-${{ data[1].transaction_amount.amount }},		
-					{{ data[1].details }}
-					<p>{{ data[1].date }}</p>
+					-${{ transaction.transaction_amount.amount }},		
+					{{ transaction.details }}
+					<p>{{ transaction.date }}</p>
 				</a-timeline-item>
 			</span>
-			<span v-if="data[2] && data[2].transaction_amount.type == 1" >
-				<a-timeline-item color="green">
-					
-									${{ data[2].transaction_amount.amount }},
-									{{ data[2].details }}
-					<p>{{ data[1].date }}</p>
-				</a-timeline-item>
-			</span><span v-else >
-				<a-timeline-item color="red">
-					-${{ data[2].transaction_amount.amount }},		
-					{{ data[2].details }}
-					<p>{{ data[2].date }}</p>
-				</a-timeline-item>
-			</span>
-			<a-timeline-item color="red">
-				New card added for order #4826321
-				<p>02 JUN 2:45 PM</p>
-			</a-timeline-item>
-			<a-timeline-item color="red">
-				Unlock folders for development
-				<p>18 MAY 1:30 PM</p>
-			</a-timeline-item>
-			<a-timeline-item color="red">
-				New order #46282344
-				<p>14 MAY 3:30 PM</p>
-			</a-timeline-item>
-			<template #pendingDot> </template>
+			</div>
 		</a-timeline>
 		
 	</a-card>
@@ -74,7 +36,7 @@
 		props: {
 			data: {
 				type: [],
-				default: () => [{details:"test", date:"test date"}],
+				default: () => [{details:"test", date:"test date", transaction_amount:{"amount": 0, "type": 1}}],
 			},
 		},
 		data() {
