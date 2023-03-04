dd4a40e5c356bdf53d83a649d4371ef4de7f2cf5
DimaRudov <6794666+DimaRudov@users.noreply.github.com>
2023-03-04 10:53:11 -0500

    adding latest transactions from the DB
    still has some kind of error

--- src/components/Cards/CardTransactionHistory.vue
+++ src/components/Cards/CardTransactionHistory.vue
@@ -1,23 +1,53 @@
 <template>
 
 	<!-- Orders History Timeline Card -->
-	<a-card :bordered="false" class="header-solid h-full" :bodyStyle="{paddingTop: '12px',}">
+	<a-card v-if="data" :bordered="false" class="header-solid h-full" :bodyStyle="{paddingTop: '12px',}">
 		<template #title>
 			<h6>Latest Transactions</h6>			
 		</template>
-		<a-timeline  >
-			<a-timeline-item color="green">
-				$2,400 - Redesign store
-				<p>09 JUN 7:20 PM</p>
-			</a-timeline-item>
-			<a-timeline-item color="green">
-				New order #3654323
-				<p>08 JUN 12:20 PM</p>
-			</a-timeline-item>
-			<a-timeline-item color="red">
-				Company server payments 
-				<p>04 JUN 3:10 PM</p>
-			</a-timeline-item>
+		<a-timeline  v-if="data " :data-source="data">
+			<span v-if="data[0] && data[0].transaction_amount.type == 1" >
+				<a-timeline-item color="green">
+					
+									${{ data[0].transaction_amount.amount }},
+									{{ data[0].details }}
+					<p>{{ data[1].date }}</p>
+				</a-timeline-item>
+			</span><span v-else >
+				<a-timeline-item color="red">
+					-${{ data[0].transaction_amount.amount }},		
+					{{ data[0].details }}
+					<p>{{ data[0].date }}</p>
+				</a-timeline-item>
+			</span>
+			<span v-if="data[1] && data[1].transaction_amount.type == 1" >
+				<a-timeline-item color="green">
+					
+									${{ data[1].transaction_amount.amount }},
+									{{ data[1].details }}
+					<p>{{ data[1].date }}</p>
+				</a-timeline-item>
+			</span><span v-else >
+				<a-timeline-item color="red">
+					-${{ data[1].transaction_amount.amount }},		
+					{{ data[1].details }}
+					<p>{{ data[1].date }}</p>
+				</a-timeline-item>
+			</span>
+			<span v-if="data[2] && data[2].transaction_amount.type == 1" >
+				<a-timeline-item color="green">
+					
+									${{ data[2].transaction_amount.amount }},
+									{{ data[2].details }}
+					<p>{{ data[1].date }}</p>
+				</a-timeline-item>
+			</span><span v-else >
+				<a-timeline-item color="red">
+					-${{ data[2].transaction_amount.amount }},		
+					{{ data[2].details }}
+					<p>{{ data[2].date }}</p>
+				</a-timeline-item>
+			</span>
 			<a-timeline-item color="red">
 				New card added for order #4826321
 				<p>02 JUN 2:45 PM</p>
@@ -41,6 +71,12 @@
 <script>
 
 	export default ({
+		props: {
+			data: {
+				type: [],
+				default: () => [{details:"test", date:"test date"}],
+			},
+		},
 		data() {
 			return {
 
