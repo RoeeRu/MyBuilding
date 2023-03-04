7698bf1e20eb0ed95189410b741c890a9e52e287
DimaRudov <6794666+DimaRudov@users.noreply.github.com>
2023-03-04 10:50:22 -0500

    moving documents action to a separate component

---
+++ src/components/Cards/CardDocumentsActions.vue
@@ -0,0 +1,85 @@
+<template>
+
+	<!-- Documents actions Card -->
+	<a-card :bordered="false" class="header-solid h-full" :bodyStyle="{paddingTop: 0,}">
+
+		<template #title>
+			<a-row type="flex" align="middle">
+
+				<a-col :span="24" :md="4" >
+					<a-button type="primary"
+						@click="showModal"
+					>
+						Add Document
+					</a-button>
+				</a-col>
+				<a-col :span="24" :md="8">
+					<!-- Header Search Input -->
+					<a-input-search class="header-search"  placeholder="Search for document…" >
+						<svg slot="prefix" width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4ZM2 8C2 4.68629 4.68629 2 8 2C11.3137 2 14 4.68629 14 8C14 9.29583 13.5892 10.4957 12.8907 11.4765L17.7071 16.2929C18.0976 16.6834 18.0976 17.3166 17.7071 17.7071C17.3166 18.0976 16.6834 18.0976 16.2929 17.7071L11.4765 12.8907C10.4957 13.5892 9.29583 14 8 14C4.68629 14 2 11.3137 2 8Z" fill="#111827"/>
+						</svg>
+					</a-input-search>
+					<!-- / Header Search Input -->
+				</a-col>
+			</a-row>
+			<MainModal
+					:visible="visible"
+					:title="modelTitle"
+				 	@handleOk="modalHandleOk"
+					:handle-cancel="modalHandleCancel"
+				>
+				<MainForm ref="formFields" :formFields="documentsInputs" :formState="formState"></MainForm>
+			</MainModal>
+		</template>
+
+	</a-card>
+	<!-- Documents actions Card -->
+
+</template>
+
+<script>
+import MainModal from '../Modal/MainModal.vue';
+import MainForm from '../Forms/MainForm.vue';
+
+
+	export default ({
+		components: {
+		  MainModal, MainForm
+		},
+		data() {
+			return {
+				visible: false,
+				modelTitle: "Add Documents",
+				documentsInputs: [
+	        		{ name: 'type', label: 'Type', placeholder:'Enter type'},
+	        		{ name: 'details', label: 'Details', placeholder:'Enter Details'},
+	        		{ name: 'upload_file', label: 'Upload', placeholder:'Upload File'},
+      	],
+				formState: {'type': '', 'details': '', 'upload_file': ''}
+			}
+		},
+		methods: {
+		  showModal() {
+		    this.visible = true
+		  },
+			modalHandleCancel() {
+				this.visible = false
+			},
+			async modalHandleOk(handleLoading) {
+				return await setTimeout(async () => {
+					handleLoading()
+					console.log(this.formState);
+					if(true) {
+						this.$refs.formFields.onFinish(true);
+						this.visible = false;
+					} else {
+						this.$refs.formFields.onFinish(false);
+					}
+					return;
+				}, 5000);
+		  },
+		}
+	})
+
+</script>
--- src/views/Documents.vue
+++ src/views/Documents.vue
@@ -8,62 +8,41 @@
 		<a-row  :gutter="24" type="flex">
 			<a-col  :span="24" class="mb-24">
 				<!-- Documents actions Card -->
-				<a-card :bordered="false" class="header-solid h-full" :bodyStyle="{paddingTop: 0,}">
-					
-					<template #title>
-						<a-row type="flex" align="middle">
-							
-							<a-col :span="24" :md="4" >
-								<a-button type="primary">
-									Add Document
-								</a-button>
-							</a-col>
-							<a-col :span="24" :md="8">
-								<!-- Header Search Input -->
-								<a-input-search class="header-search"  placeholder="Search for document…" >
-									<svg slot="prefix" width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path fill-rule="evenodd" clip-rule="evenodd" d="M8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4ZM2 8C2 4.68629 4.68629 2 8 2C11.3137 2 14 4.68629 14 8C14 9.29583 13.5892 10.4957 12.8907 11.4765L17.7071 16.2929C18.0976 16.6834 18.0976 17.3166 17.7071 17.7071C17.3166 18.0976 16.6834 18.0976 16.2929 17.7071L11.4765 12.8907C10.4957 13.5892 9.29583 14 8 14C4.68629 14 2 11.3137 2 8Z" fill="#111827"/>
-									</svg>
-								</a-input-search>
-								<!-- / Header Search Input -->
-							</a-col>
-						</a-row>
-					</template>
-				
-				</a-card>
+				<CardCardDocumentsActions></CardCardDocumentsActions>
 				<!-- Documents actions Card -->
 
 			</a-col>
 
 		</a-row>
-		<!-- Transactions Table -->
+		<!-- Documents Table -->
 		<a-row :gutter="24" type="flex">
 
-			<!-- Transactions Table Column -->
+			<!-- Documents Table Column -->
 			<a-col :span="24" class="mb-24">
 
-				<!-- Transactions Table Card -->
+				<!-- Documents Table Card -->
 				<CardDocumentsTable
 					:data="table1Data"
 					:columns="table1Columns"
 				></CardDocumentsTable>
-				<!-- / Transactions Table Card -->
+				<!-- / Documents Table Card -->
 
 			</a-col>
-			<!-- / Transactions Table Column -->
+			<!-- / Documents Table Column -->
 
 		</a-row>
-		<!-- / Transactions Table -->
+		<!-- / Documents Table -->
 
 	</div>
 </template>
 
 <script>
 
-	// "Transactions" table component.
+	// "Documents" table component.
 	import CardDocumentsTable from '../components/Cards/CardDocumentsTable.vue';
-	
-	// "Transactions" table list of columns and their properties.
+	import CardCardDocumentsActions from '../components/Cards/CardDocumentsActions.vue';
+
+	// "Documents" table list of columns and their properties.
 	const table1Columns = [
 		{
 			title: 'Document Name',
@@ -76,16 +55,16 @@
 			dataIndex: 'type',
 			class: 'font-semibold text-muted text-sm',
 		},
-		{
-			title: 'Date Uploaded',
-			dataIndex: 'date',
-			class: 'font-semibold text-muted text-sm',
-		},
 		
 		
 		{
 			title: 'Details',
 			dataIndex: 'details',
+			class: 'font-semibold text-muted text-sm',
+		},
+		{
+			title: 'Date Uploaded',
+			dataIndex: 'date',
 			class: 'font-semibold text-muted text-sm',
 		},
 		
@@ -97,7 +76,7 @@
 		},
 	];
 
-	// "Transactions" table list of rows and their properties.
+	// "Documents" table list of rows and their properties.
 	const table1Data = [
 		{
 			key: '1',
@@ -125,6 +104,7 @@
 	export default ({
 		components: {
 	CardDocumentsTable,
+	CardCardDocumentsActions,
 },
 		data() {
 			return {
