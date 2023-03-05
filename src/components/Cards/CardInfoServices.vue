cfdf06d3f629910210c7d9f11c23a6908edc9c70
DimaRudov <6794666+DimaRudov@users.noreply.github.com>
2023-03-04 20:53:25 -0500

    service page can send  messages

--- src/components/Cards/CardInfoServices.vue
+++ src/components/Cards/CardInfoServices.vue
@@ -9,12 +9,12 @@
 					<p>{{ details.description }}</p>
 				</div>
 				<div class="card-footer">
-					<a href="#" size="small">
+					<a-button slot="actions" type="link" @click="sendEmail(details.email_body, buildingInfo.address, userInfo.name)">				
 						<span>{{ details.action }}</span>
 						<svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path fill-rule="evenodd" clip-rule="evenodd" d="M7.29289 14.7071C6.90237 14.3166 6.90237 13.6834 7.29289 13.2929L10.5858 10L7.29289 6.70711C6.90237 6.31658 6.90237 5.68342 7.29289 5.29289C7.68342 4.90237 8.31658 4.90237 8.70711 5.29289L12.7071 9.29289C13.0976 9.68342 13.0976 10.3166 12.7071 10.7071L8.70711 14.7071C8.31658 15.0976 7.68342 15.0976 7.29289 14.7071Z" fill="#111827"/>
 						</svg>
-					</a>
+				</a-button>
 				</div>
 			</a-col>
 			<a-col class="col-img" :span="24" :xl="12">
@@ -40,13 +40,42 @@
 						description: "Long text about the service",
 						image_url: "https://via.placeholder.com/600x400.png?text=Image",
 						action: "Learn More",
+						email_body: "Hello"
 					}
 				}
-			}},
+			},
+			buildingInfo: {
+				type: Object,
+				default: () => {
+					return {
+						name: "Building Name",
+						address: "Building Address",
+					}
+				}
+			},
+			userInfo: {
+				type: Object,
+				default: () => {
+					return {
+						name: "User Name",
+						email: "Email",
+					}
+				}
+			},
+		},
+			
 		data() {
 			return {
 			}
 		},
+		methods: {
+			sendEmail(email_body, address, name) {
+				console.log('send invite')
+				var formattedBody = email_body+"\n"+name+"\n"+address;
+				var formattedSubject = "Join Your Building Page @ Domos";
+				window.open("mailto:hello@joindomos.com?subject="+encodeURIComponent(formattedSubject)+"&body="+encodeURIComponent(formattedBody), '_blank').focus();
+			}
+		}
 	})
 
 </script>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -13,12 +13,16 @@
 			<a-col :span="24" :lg="12" class="mb-24">
                 <CardInfoServices 
                     :details = "generalCard"
+                    :buildingInfo = "buildingInfo"
+                    :userInfo = "userInfo"
                 >
                 </CardInfoServices>
             </a-col>
             <a-col :span="24" :lg="12" class="mb-24" >
                 <CardInfoServices 
                     :details = "superCard"
+                    :buildingInfo = "buildingInfo"
+                    :userInfo = "userInfo"
                 >
                 </CardInfoServices>
             </a-col>
@@ -27,12 +31,16 @@
 			<a-col :span="24" :lg="12" class="mb-24">
                 <CardInfoServices 
                     :details = "vendorsCard"
+                    :buildingInfo = "buildingInfo"
+                    :userInfo = "userInfo"
                 >
                 </CardInfoServices>
             </a-col>
             <a-col :span="24" :lg="12" class="mb-24" >
                 <CardInfoServices 
                     :details = "LegalCard"
+                    :buildingInfo = "buildingInfo"
+                    :userInfo = "userInfo"
                 >
                 </CardInfoServices>
             </a-col>
@@ -41,12 +49,16 @@
             <a-col :span="24" :lg="12" class="mb-24" >
                 <CardInfoServices 
                     :details = "InsuranceCard"
+                    :buildingInfo = "buildingInfo"
+                    :userInfo = "userInfo"
                 >
                 </CardInfoServices>
             </a-col>
             <a-col :span="24" :lg="12" class="mb-24" >
                 <CardInfoServices 
                     :details = "FinanceCard"
+                    :buildingInfo = "buildingInfo"
+                    :userInfo = "userInfo"
                 >
                 </CardInfoServices>
             </a-col>
@@ -58,6 +70,8 @@
 <script>
     // Information card 1.
     import CardInfoServices from '../components/Cards/CardInfoServices.vue' ;
+	import { mapActions } from 'vuex'
+	import { mapState } from 'vuex'
 
     
     export default ({
@@ -70,12 +84,14 @@
                     above_title: "Online",
                     title: "General Inquiry",
                     action: "Send a message",
+                    email_body: "Hello Domos team, \n \nPlease advise with the following:\n\n\nThank you, ",
                     image_url: "images/general_inq.jpeg",
                     description: "Have a question about your building but not sure who to ask? Schedule a quick call with a Domos expert."
                 },
                 superCard: {
                     above_title: "On-site",
                     title: "Super On Demand",
+                    email_body: "Hello Domos team, \n \nWe would like to use your on-demand super/handyman services. Here are some details about our needs:\nRequired scope of work: \nOne time / reccuring\nPreferred dates / days:\nPreferred hours of visit: \n\n\nThank you, ",
                     action: "Get a quote",
                     image_url: "images/super.jpeg",
                     description: "Schedule a one time visit from a Super to help you with your building maintenenace tasks."
@@ -84,19 +100,22 @@
                     above_title: "On-site",
                     title: "Vendors Network",
                     action: "Get a quote",
+                    email_body: "Hello Domos team, \n \nWe would need to get in touch with the following vendor type: \nHere are some details about the project we need help with: \nPlease see attached photos (if relevant)\n\n\nThank you, ",
                     image_url: "images/vendors.jpeg",
                     description: "Need a plummber / roofer / other vendor? Tell us what's the issue and we will match you with an expert."
                 },
                 LegalCard: {
                     above_title: "Online",
                     title: "Legal Experts",
-                    action: "Get a quote",
+                    email_body: "Hello Domos team, \n \nWe would like to get advise regarding the following legal issue:\n\n\nThank you, ",
+                    action: "Share details",
                     image_url: "images/legal.jpeg",
                     description: "Schedule a quick call with a legal expert to help you with your legal questions."
                 },
                 InsuranceCard: {
                     above_title: "Online",
                     title: "Insurance Experts",
+                    email_body: "Hello Domos team, \n \nWe would like to get an offer for building / home / other insurance. Some additional details about what we are looking for:\n\n\nThank you, ",
                     action: "Try for free",
                     image_url: "images/insurance.jpeg",
                     description: "Schedule a quick call with a legal expert to help you with your legal questions."
@@ -104,13 +123,32 @@
                 FinanceCard: {
                     above_title: "Online",
                     title: "Financial Advice",
+                    email_body: "Hello Domos team, \n \nPlease put us in touch with a financial expert regarding the following issue:\n\n\nThank you, ",
                     action: "Get a quote",
                     image_url: "images/tax.jpeg",
                     description: "Speak with an expert regarding taxes and and other financial issues."
                 },
                 
             }
-        }
+        },
+		computed: {
+			...mapState({
+				userInfo: state => state.profile.userInfo,
+				buildingInfo: state => state.building.buildingInfo,
+			})
+		},
+
+		async mounted() {
+			await this.getBuildingInformation();
+			await this.getUserData();
+		},
+		methods: {
+			...mapActions({
+				getBuildingInformation: 'building/getBuildingInformation',
+				getUserData: 'profile/getUserData'
+			})
+		  
+		}
     }
 
     )
