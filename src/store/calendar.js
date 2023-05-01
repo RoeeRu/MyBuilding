a8a6404fdf50dd135574de5a853152ccb5d64fb4
DimaRudov <6794666+DimaRudov@users.noreply.github.com>
2023-05-01 13:20:39 -0400

    calendar works with API

--- src/store/calendar.js
+++ src/store/calendar.js
@@ -1,8 +1,14 @@
-import 'vuetify/dist/vuetify.min.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import DaySpanVuetify from 'dayspan-vuetify'
+import { getCalendar, replaceCalendar } from '@/Api/calendar.js';
+
+import 'vuetify/dist/vuetify.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'dayspan-vuetify/dist/lib/dayspan-vuetify.min.css'
-import DaySpanVuetify from 'dayspan-vuetify'
-import Vue from 'vue'
+
+Vue.config.productionTip = false
+
 
 
 Vue.use( DaySpanVuetify, {
@@ -10,17 +16,8 @@ Vue.use( DaySpanVuetify, {
     data: {
       // data or computed to override
     },
-    computed:
-  {
-    fullscreenPopovers()
-    {
-      return this.$vuetify.breakpoint.xl;
-    },
-
-    fullscreenDialogs()
-    {
-      return this.$vuetify.breakpoint.xl;
-    }
+    computed:{
+    // computed to override
   },
     methods: {
       // methods to override
@@ -30,10 +27,35 @@ Vue.use( DaySpanVuetify, {
 
 
 export default {
-    namespaced: true,
-    state () {
-        return {
-            events: [],
-        }
+  namespaced: true,
+  state () {
+    return {
+      calendarEvents: [],
     }
+  },
+  mutations: {
+    calendarInfo (state, calendarEvents) {
+      state.calendarEvents = calendarEvents
+    },
+  },
+  actions: {
+    async getCalendar({ state, rootState, commit }, calendarPayload) {
+      let res = await getCalendar(rootState.auth.user.accessToken, calendarPayload);
+      if(!res.status) {
+        console.log("faield", res.data);
+        return false;
+      }
+      commit('calendarInfo', res.data);
+    },
+    async replaceCalendar({ state, rootState, commit }, calendarPayload) {
+      let res = await replaceCalendar(rootState.auth.user.accessToken, calendarPayload);
+      if(!res.status) {
+        console.log("res.status faield", res.status);
+        return false;
+      }
+      console.log('got new data', res.data)
+      commit('calendarInfo', res.data)
+      return true;
+    }
+  },
 }
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -5,9 +5,10 @@
 		:calendar="calendar"
 		:read-only="readOnly"
 		@change="saveState">
+		
   
 		<template slot="title">
-		  DaySpan
+		  Domos
 		</template>
   
   
@@ -59,6 +60,7 @@
   <script>
   import { Calendar, Weekday, Month } from 'dayspan';
   import Vue from 'vue';
+  import { mapState, mapActions  } from 'vuex';
   
   
   export default {
@@ -72,192 +74,40 @@
 	  defaultEvents: [
 		{
 		  data: {
-			title: 'Weekly Meeting',
-			color: '#3F51B5'
-		  },
-		  schedule: {
-			dayOfWeek: [Weekday.MONDAY],
-			times: [9],
-			duration: 30,
-			durationUnit: 'minutes'
-		  }
-		},
-		{
-		  data: {
-			title: 'First Weekend',
-			color: '#4CAF50'
-		  },
-		  schedule: {
-			weekspanOfMonth: [0],
-			dayOfWeek: [Weekday.FRIDAY],
-			duration: 3,
-			durationUnit: 'days'
-		  }
-		},
-		{
-		  data: {
-			title: 'End of Month',
-			color: '#000000'
-		  },
-		  schedule: {
-			lastDayOfMonth: [1],
-			duration: 1,
-			durationUnit: 'hours'
-		  }
-		},
-		{
-		  data: {
-			title: 'Mother\'s Day',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.MAY],
-			dayOfWeek: [Weekday.SUNDAY],
-			weekspanOfMonth: [1]
-		  }
-		},
-		{
-		  data: {
-			title: 'New Year\'s Day',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.JANUARY],
-			dayOfMonth: [1]
-		  }
-		},
-		{
-		  data: {
-			title: 'Inauguration Day',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.JANUARY],
-			dayOfMonth: [20]
-		  }
-		},
-		{
-		  data: {
-			title: 'Martin Luther King, Jr. Day',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.JANUARY],
-			dayOfWeek: [Weekday.MONDAY],
-			weekspanOfMonth: [2]
-		  }
-		},
-		{
-		  data: {
-			title: 'George Washington\'s Birthday',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.FEBRUARY],
-			dayOfWeek: [Weekday.MONDAY],
-			weekspanOfMonth: [2]
-		  }
-		},
-		{
-		  data: {
-			title: 'Memorial Day',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.MAY],
-			dayOfWeek: [Weekday.MONDAY],
-			lastWeekspanOfMonth: [0]
-		  }
-		},
-		{
-		  data: {
-			title: 'Independence Day',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.JULY],
-			dayOfMonth: [4]
-		  }
-		},
-		{
-		  data: {
-			title: 'Labor Day',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.SEPTEMBER],
-			dayOfWeek: [Weekday.MONDAY],
-			lastWeekspanOfMonth: [0]
-		  }
-		},
-		{
-		  data: {
-			title: 'Columbus Day',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.OCTOBER],
-			dayOfWeek: [Weekday.MONDAY],
-			weekspanOfMonth: [1]
-		  }
-		},
-		{
-		  data: {
-			title: 'Veterans Day',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.NOVEMBER],
-			dayOfMonth: [11]
-		  }
-		},
-		{
-		  data: {
-			title: 'Thanksgiving Day',
-			color: '#2196F3',
-			calendar: 'US Holidays'
-		  },
-		  schedule: {
-			month: [Month.NOVEMBER],
-			dayOfWeek: [Weekday.THURSDAY],
-			weekspanOfMonth: [3]
-		  }
-		},
-		{
-		  data: {
 			title: 'Christmas Day',
 			color: '#2196F3',
 			calendar: 'US Holidays'
 		  },
 		  schedule: {
-			month: [Month.DECEMBER],
+			month: [Month.APRIL],
 			dayOfMonth: [25]
 		  }
 		}
 	  ]
 	}),
+	computed: {
+			...mapState(
+				{ calendarEvents: state => state.calendar.calendarEvents,
+				BuildingID: state => state.auth.user.building_id }),
+		},
   
-	mounted()
+	async mounted()
 	{
-	  window.app = this.$refs.app;
+		
+		window.analytics.page('Calendar')
+		await this.getCalendar({building_id: this.BuildingID});
   
-	  this.loadState();
+		await this.loadState();
   
   
 	},
   
 	methods:
 	{
+		...mapActions({
+			getCalendar: 'calendar/getCalendar',
+			replaceCalendar: 'calendar/replaceCalendar',
+		}),
   
 	  getCalendarTime(calendarEvent)
 	  {
@@ -284,21 +134,27 @@
 		let state = this.calendar.toInput(true);
 		let json = JSON.stringify(state);
   
-		localStorage.setItem(this.storeKey, json);
+		//localStorage.setItem(this.storeKey, json);
+
+		this.replaceCalendar({newCalendar: state, building_id: this.BuildingID});
+		
 	  },
   
 	  loadState()
 	  {
 		let state = {};
+		
   
 		try
 		{
-		  let savedState = JSON.parse(localStorage.getItem(this.storeKey));
+		 
   
-		  if (savedState)
+		  if (this.calendarEvents)
 		  {
-			state = savedState;
+			console.log("this.calendarEvents", this.calendarEvents[0])
+			state = this.calendarEvents[0];
 			state.preferToday = false;
+			console.log("state", state)
 		  }
 		}
 		catch (e)
@@ -309,7 +165,7 @@
   
 		if (!state.events || !state.events.length)
 		{
-		  state.events = this.defaultEvents;
+		  state.events = [] // this.defaultEvents;
 		}
   
 		state.events.forEach(ev =>
